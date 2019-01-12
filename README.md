fb51dc9cc58c76eadea4b5758efb4f930d468e78
Dilan Madhusanka <dilanm@alliontechnologies.local>
2019-01-12 16:05:00 +0530

    Basic structure of the home page using only HTML.

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 # php_crud01
-This is first crud application with HTML,CSS,JavaScript and PHP.
 
+This is first crud application with HTML,CSS,JavaScript and PHP.
 This contains no login page. Only create, read, update, delete data with mysql.
---
+++ index.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>PHP crud 01</title>
+</head>
+<body>
+	<h1>PHP crud 01</h1><hr/>
+	<p>This is first crud application using HTML, CSS, JavaScript, PHP with MySQL.</p>
+	<button>Add Movie</button>
+	<table>
+		<caption>Movies Library</caption>
+		<thead>
+			<tr>
+				<th>ID</th>
+				<th>Name</th>
+				<th>Category</th>
+				<th>Published date</th>
+				<th>Producer</th>
+				<th>Main Actor</th>
+				<th>Main Actress</th>
+				<th>Actions</th>
+			</tr>
+		</thead>
+		<tbody></tbody>
+		<tfoot>
+			<tr>
+				<td colspan="8"><small>Created by kadm0128</small></td>
+			</tr>
+		</tfoot>
+	</table>
+</body>
+</html>
